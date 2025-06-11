e804488b0cf5bfe634d93b55cbe8392bec573717
Hassan-Akhtyr <hassanakhtyr@gmail.com>
2025-06-11 19:00:05 +0500

    Add particles.js and script.js for enhanced particle effects
    - Introduced particles.js library for advanced particle animations.
    - Implemented script.js to manage canvas rendering and particle behavior.
    - Added functionality for dynamic particle generation and interaction.
    - Integrated sliders for user control over particle sphere radius and size.

---
+++ UI/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $("#animated-text").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn",
+    },
+    out: {
+      effect: "bounceOut",
+    },
+  });
+});
